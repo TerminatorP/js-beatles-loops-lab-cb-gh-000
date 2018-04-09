cd89a8e2560e5b3047152db3403a4406d55a0ce5
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-09 06:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return output;
 }
+
+function johnLennonFacts(facts) {
+  let output = [];
+  let c = 0;
+  while (c < facts.length) {
+    output.push(`${facts[c]}!!!`);
+    c++;
+  }
+  return output;
+}
+
+function iLoveTheBeatles(num) {
+  let output = [];
+  do {
+    output.push('I love the Beatles!');
+    num++;
+  } while(num < 15);
+  return output;
+}
